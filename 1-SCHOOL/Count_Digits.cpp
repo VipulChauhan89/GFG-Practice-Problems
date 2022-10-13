7904130f68b321dff26c0760c571f9c0dd38ab7b
VipulChauhan89 <vipulchauhan548@gmail.com>
2022-10-13 18:21:37 +0530

    Create Count_Digits.cpp

---
+++ 1-SCHOOL/Count_Digits.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N)
+    {
+        int num=N,count=0;
+        while(num!=0)
+        {
+            int digit=num%10;
+            num/=10;
+            if(digit >0 && N%digit==0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
