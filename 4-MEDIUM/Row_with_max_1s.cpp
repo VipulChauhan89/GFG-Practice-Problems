87be99311bb3c3273a7e45171437014a9fd9c535
VipulChauhan89 <vipulchauhan548@gmail.com>
2022-10-26 16:01:41 +0530

    Create Row_with_max_1s.cpp

---
+++ 4-MEDIUM/Row_with_max_1s.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) 
+	{
+	    int c1=0,c2=0,ans=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        c1=0;
+	        for(int j=0;j<m;j++)
+	        {
+	            if(arr[i][j]==1)
+	            {
+	                c1++;
+	            }
+	        }
+	        if(c1>c2)
+	        {
+	            c2=c1;
+	            ans=i;
+	        }
+	    }
+	    return (c2==0)?-1:ans;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector< vector<int> > arr(n,vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin>>arr[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr, n, m);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
