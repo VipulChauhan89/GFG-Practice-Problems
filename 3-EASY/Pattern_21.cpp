ad0149d850794f8730b0e35ac76d8654e43e3792
VipulChauhan89 <vipulchauhan548@gmail.com>
2022-11-23 16:30:28 +0530

    Create Pattern_21.cpp

---
+++ 3-EASY/Pattern_21.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printSquare(int n) 
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(i==0||i==n-1)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    cout<<"*";
+                }
+                cout<<endl;
+            }
+            else
+            {
+                cout<<"*";
+                for(int j=2;j<n;j++)
+                {
+                    cout<<" ";
+                }
+                cout<<"*"<<endl;  
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printSquare(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
